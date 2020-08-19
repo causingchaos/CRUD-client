c9db160297359380f18fcce9a7320ef7b0d630f6
Chaos <justin.db83@gmail.com>
2020-08-18 19:30:20 -0600

    updating client code with cookie setting buttons

--- scripts/shared.js
+++ scripts/shared.js
@@ -1,3 +1,11 @@
+console.log("hello world")
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('btnSet').addEventListener('click', fSet);
+  document.getElementById('btnAdd').addEventListener('click',fAdd);
+  let pre = document.getElementById('output');
+  pre.textContent = document.cookie;
+})
+
 const API_URL = getHostURL();
 
 function getHostURL() {
@@ -19,4 +27,29 @@ function getUserFromForm(){
   }
 
   return user;
+}
+
+function fSet() {
+  let key = 'score';
+  let value = encodeURIComponent(
+    (Math.floor(Math.random() * 123456) + 123456).toString()
+  );
+  let thirty = 60 * 60 * 24 * 30;
+  document.cookie = `${key}=${value};path=/;max-age=${thirty};`; //one cookie at a time
+  document.getElementById('output').textContent = document.cookie;
+}
+
+function fAdd(){
+  console.log(API_URL);
+  let url = `${API_URL}/set`;
+  let req = new Request(url, {
+    mode: 'cors',   // just a safe guard indicating our intentions of what to allow
+    credentials: 'include', //when will the cookies and authorization header be sent
+  });
+  fetch(req)
+    .then((resp) => {
+      resp.headers.forEach((val, key) => {
+        console.log(key,val);
+      });
+    });
 }
