578d699d9c6c8ee35b57cc877beb3cfc91719d6b
causingchaos <none>
2020-08-20 01:48:12 -0600

    updating code

--- scripts/shared.js
+++ scripts/shared.js
@@ -2,6 +2,7 @@ console.log("hello world")
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('btnSet').addEventListener('click', fSet);
   document.getElementById('btnAdd').addEventListener('click',fAdd);
+  document.getElementById('btnDelete').addEventListener('click',fDelete);
   let pre = document.getElementById('output');
   pre.textContent = document.cookie;
 })
@@ -37,6 +38,14 @@ function fSet() {
   let thirty = 60 * 60 * 24 * 30;
   document.cookie = `${key}=${value};path=/;max-age=${thirty};`; //one cookie at a time
   document.getElementById('output').textContent = document.cookie;
+  /**
+   ;path= absolute path. current path by default
+   ;domain=sub.example current domain by default
+   ;max-age= seconds 60*60*24*30 30 days
+   ;expires= UTC date end of current session by default
+   ;secure=true;
+   ;same-site=Strict | Lax
+   **/
 }
 
 function fAdd(){
@@ -46,10 +55,43 @@ function fAdd(){
     mode: 'cors',   // just a safe guard indicating our intentions of what to allow
     credentials: 'include', //when will the cookies and authorization header be sent
   });
+  //return resp.json();
+
   fetch(req)
     .then((resp) => {
       resp.headers.forEach((val, key) => {
         console.log(key,val);
       });
+      let cookie = resp.headers.get('set-cookie');
+      console.log('set-cookie header value', cookie);
+
+      return resp.json();
     });
+    /**
+      credentials: "same-origin", "omit" (never send), "include" (always send)  -- cookies, Authorization (combined)
+      mode: "cors", "no-cors", "same-origin"
+      default 7 visible headers for CORS requests
+        cache-control, content-language, content-length, content-type, expires, last_modified, pragam
+
+        set-cookie and set-cookie2 will NEVER be accessible to JS4
+     */
+}
+
+function fDelete() {
+  let url = `${API_URL}/delete/token`;
+  let req = new Request(url, {
+    mode: 'cors',   // cors = cros origin request
+    credentials: 'include'
+  });
+  fetch(req)
+    .then((resp) => {
+      resp.headers.forEach((val, key) => {
+        console.log(key,val);
+      });
+      let cookie = resp.headers.get('set-cookie');
+      console.log(cookie);
+      
+      return resp.json();
+    })
+    
 }
